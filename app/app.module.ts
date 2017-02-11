b231852dd43df8ba6f1851906553d9accf8229e7
Steve Green <Steve Green>
2017-02-11 11:27:39 +0000

    Creating and Communicating

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,14 +2,21 @@ import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 
 import { EventsAppComponent } from './events-app.component'
+import { EventsListComponent } from './events/events-lists.component'
+import { EventThumbnailComponent } from './events/event-thumbnail.component'
+import { NavbarComponent } from './nav/navbar.component'
 
 @NgModule({
     imports: [BrowserModule],
-    declarations: [EventsAppComponent],
+    declarations: [ EventsAppComponent,
+                    EventsListComponent,
+                    EventThumbnailComponent,
+                    NavbarComponent
+                ],
     bootstrap: [EventsAppComponent]
 
 })
 
 export class AppModule {
-
+   
 }
--- app/events-app.component.ts
+++ app/events-app.component.ts
@@ -3,7 +3,7 @@ import { Component} from '@angular/core'
 
 @Component({
     selector: 'events-app',
-    template: '<h2>hello world</h2>'
+    template: '<nav-bar></nav-bar><events-list></events-list>'
 })
 
 export class EventsAppComponent {
